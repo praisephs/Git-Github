a2e9ae99b422a80e2e24b3c6aff4401e93e53823
praisephs <uyioboite@gmail.com>
2023-10-15 19:53:19 +0100

    Added my full name and email address to mine.py file

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "Uyiosa Praise Oboite"
+email = 'uyioboite@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
